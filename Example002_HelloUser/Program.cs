7328043b23088b5854a98fa03c006711f4d580d8
Alex <Lecov@yandex.ru>
2022-11-20 00:07:32 +0300

    HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ваше имя!");
+string User = Console.ReadLine();
+Console.WriteLine("Привет " + User);
+Console.Write("Привет ");
+Console.Write(User);
+
