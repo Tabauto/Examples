3bfafc34e9243e3c2c3a657769c6cf234fe41289
Alex <Lecov@yandex.ru>
2022-11-27 09:00:31 +0300

    заново еще раз

--- Task_002/obj/Release/net7.0/Task_002.AssemblyInfo.cs
+++ Task_002/obj/Release/net7.0/Task_002.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Example002_HelloUser")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task_002")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Example002_HelloUser")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Example002_HelloUser")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task_002")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task_002")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- Task_003/obj/Debug/net7.0/Task_003.AssemblyInfo.cs
+++ Task_003/obj/Debug/net7.0/Task_003.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Example002_HelloUser")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Task_003")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Example002_HelloUser")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Example002_HelloUser")]
+[assembly: System.Reflection.AssemblyProductAttribute("Task_003")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Task_003")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
